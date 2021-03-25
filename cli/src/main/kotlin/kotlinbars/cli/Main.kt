505d29c6204660fbec8621c9d38e9f2d080a0316
James Ward <james@jamesward.com>
2021-03-25 09:48:26 -0600

    fix web app starting

--- cli/src/main/kotlin/kotlinbars/cli/Main.kt
+++ cli/src/main/kotlin/kotlinbars/cli/Main.kt
@@ -3,12 +3,21 @@ package kotlinbars.cli
 import kotlinbars.common.Bar
 import kotlinx.coroutines.runBlocking
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.nativex.hint.MethodHint
+import org.springframework.nativex.hint.TypeHint
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBodilessEntity
 import org.springframework.web.reactive.function.client.awaitBody
 
+// https://github.com/spring-projects-experimental/spring-native/issues/663
+@TypeHint(
+    types = [SpringApplication::class],
+    methods = [MethodHint(name = "setWebApplicationType", parameterTypes = [WebApplicationType::class])]
+)
 @SpringBootApplication
 class BarsCommandLine : CommandLineRunner {
 
@@ -22,8 +31,7 @@ class BarsCommandLine : CommandLineRunner {
 
             if (bars.isEmpty()) {
                 println("\nNo Bars")
-            }
-            else {
+            } else {
                 println("\nBars:")
                 bars.forEach { println("  ${it.name}") }
             }
