ae2ac3d388ccd41a3e7c9acc42b7b3c214abce9d
James Ward <james@jamesward.com>
2021-03-26 00:27:12 -0600

    provide a way to init the db

--- server/src/main/kotlin/kotlinbars/server/Main.kt
+++ server/src/main/kotlin/kotlinbars/server/Main.kt
@@ -3,11 +3,21 @@ package kotlinbars.server
 import kotlinbars.common.Bar
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.core.io.Resource
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.await
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -33,6 +43,27 @@ class WebApp(val barRepo: BarRepo) {
 
 }
 
+@Configuration(proxyBeanMethods = false)
+class InitConfiguration {
+
+    @Bean
+    @Profile("init")
+    fun commandLineRunner(databaseClient: DatabaseClient, @Value("classpath:init.sql") initSql: Resource): CommandLineRunner {
+        return CommandLineRunner {
+            val lines = initSql.inputStream.bufferedReader().use { it.readText() }
+            runBlocking {
+                databaseClient.sql(lines).await()
+            }
+        }
+    }
+
+}
+
 fun main(args: Array<String>) {
-    runApplication<WebApp>(*args)
+    runApplication<WebApp>(*args) {
+        if (args.contains("init")) {
+            webApplicationType = WebApplicationType.NONE
+            setAdditionalProfiles("init")
+        }
+    }
 }
