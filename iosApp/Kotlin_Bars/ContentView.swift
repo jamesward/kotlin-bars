8171e5fd9275669f3730c0f26f6fa43bbea2b9f9
James Ward <james@jamesward.com>
2022-07-20 12:59:03 -0600

    add license

--- iosApp/Kotlin_Bars/ContentView.swift
+++ iosApp/Kotlin_Bars/ContentView.swift
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import SwiftUI
 import KotlinbarsCommon
 import KotlinbarsRpc
@@ -5,7 +21,7 @@ import KotlinbarsRpc
 @MainActor
 protocol BarsViewModel: ObservableObject {
     var bars: [CommonBar] { get }
-    
+
     func refresh() async throws
     func add(bar: CommonBar) async throws
 }
@@ -15,38 +31,38 @@ class LiveBarsViewModel: BarsViewModel {
     @Published var bars: [CommonBar] = []
 
     let barsRPC: BarsRPC
-    
+
     required init(barsUrl: String) {
         NSLog("init start")
         barsRPC = BarsRPC(barsUrl: barsUrl)
         NSLog("init done")
     }
-    
+
     func refresh() async throws {
         NSLog("do refresh")
         bars = try await barsRPC.fetchBars()
     }
-    
+
     func add(bar: CommonBar) async throws {
         try await barsRPC.addBar(bar: bar)
         try await refresh()
     }
-    
+
     deinit {
         barsRPC.close()
     }
 }
- 
+
 
 struct BarsView<ViewModel>: View where ViewModel: BarsViewModel {
     @ObservedObject var viewModel: ViewModel
-    
+
     @State private var barName: String = ""
-    
+
     var body: some View {
         VStack {
             Text("Bars:")
-            
+
             List(viewModel.bars, id: \.id) { bar in
                 Text(bar.name).padding()
             }
@@ -57,7 +73,7 @@ struct BarsView<ViewModel>: View where ViewModel: BarsViewModel {
             .refreshable {
                 try? await viewModel.refresh()
             }
-            
+
             TextField(
                     "New Bar",
                     text: $barName
@@ -74,7 +90,7 @@ struct BarsView<ViewModel>: View where ViewModel: BarsViewModel {
 
         }
     }
-    
+
 }
 
 struct BarsView_Previews: PreviewProvider {
@@ -83,16 +99,16 @@ struct BarsView_Previews: PreviewProvider {
             CommonBar(id: 1, name: "Foo"),
             CommonBar(id: 2, name: "Bar")
         ]
-        
+
         func refresh() async throws {
             // do nothing
         }
-        
+
         func add(bar: CommonBar) async throws {
             bars.append(bar) // todo: increment id
         }
     }
-    
+
     static var previews: some View {
         BarsView<PreviewBarsViewModel>(viewModel: PreviewBarsViewModel())
     }
