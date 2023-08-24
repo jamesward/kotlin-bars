09ee472e4fa95e70f3e0b4dc6b93867c9323342e
James Ward <james@jamesward.com>
2023-08-24 09:57:01 -0600

    downbump kotlin for compose multiplatform compat

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
     id("org.springframework.boot")             version "3.1.2" apply false
     id("io.spring.dependency-management")      version "1.1.3" apply false
     id("org.graalvm.buildtools.native")        version "0.9.24" apply false
-    kotlin("android")                          version "1.9.10" apply false
-    kotlin("jvm")                              version "1.9.10" apply false
-    kotlin("multiplatform")                    version "1.9.10" apply false
-    kotlin("plugin.serialization")             version "1.9.10" apply false
-    kotlin("plugin.spring")                    version "1.9.10" apply false
+    kotlin("android")                          version "1.9.0" apply false
+    kotlin("jvm")                              version "1.9.0" apply false
+    kotlin("multiplatform")                    version "1.9.0" apply false
+    kotlin("plugin.serialization")             version "1.9.0" apply false
+    kotlin("plugin.spring")                    version "1.9.0" apply false
 }
 
 allprojects {
